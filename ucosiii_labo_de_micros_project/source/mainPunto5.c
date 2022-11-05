f0f349300f0c594fd1d074e4128ea4757f90370d
cutignolai <58371858+cutignolai@users.noreply.github.com>
2022-11-05 16:27:52 -0300

    Nuevos archivos

--- ucosiii_labo_de_micros_project/source/mainPunto5.c
+++ ucosiii_labo_de_micros_project/source/mainPunto5.c
@@ -1,61 +1,95 @@
 #include "hardware.h"
-#include  <os.h>
+#include <os.h>
+#include <stddef.h>		//NULL definition
 
-/* LEDs */
+
+/*********************************** LEDS ***********************************/
+// Defino el led ROJO
 #define LED_R_PORT            PORTB
 #define LED_R_GPIO            GPIOB
-#define LED_G_PORT            PORTE
-#define LED_G_GPIO            GPIOE
-#define LED_B_PORT            PORTB
-#define LED_B_GPIO            GPIOB
 #define LED_R_PIN             22
-#define LED_G_PIN             26
-#define LED_B_PIN             21
-#define LED_B_ON()           (LED_B_GPIO->PCOR |= (1 << LED_B_PIN))
-#define LED_B_OFF()          (LED_B_GPIO->PSOR |= (1 << LED_B_PIN))
-#define LED_B_TOGGLE()       (LED_B_GPIO->PTOR |= (1 << LED_B_PIN))
-#define LED_G_ON()           (LED_G_GPIO->PCOR |= (1 << LED_G_PIN))
-#define LED_G_OFF()          (LED_G_GPIO->PSOR |= (1 << LED_G_PIN))
-#define LED_G_TOGGLE()       (LED_G_GPIO->PTOR |= (1 << LED_G_PIN))
 #define LED_R_ON()           (LED_R_GPIO->PCOR |= (1 << LED_R_PIN))
 #define LED_R_OFF()          (LED_R_GPIO->PSOR |= (1 << LED_R_PIN))
 #define LED_R_TOGGLE()       (LED_R_GPIO->PTOR |= (1 << LED_R_PIN))
 
-/* Task Start */
+// Defino el led AZUL
+#define LED_B_PORT            PORTB
+#define LED_B_GPIO            GPIOB
+#define LED_B_PIN             21
+#define LED_B_ON()           (LED_B_GPIO->PCOR |= (1 << LED_B_PIN))
+#define LED_B_OFF()          (LED_B_GPIO->PSOR |= (1 << LED_B_PIN))
+#define LED_B_TOGGLE()       (LED_B_GPIO->PTOR |= (1 << LED_B_PIN))
+
+
+// Defino el led VERDE
+#define LED_G_PORT            PORTE
+#define LED_G_GPIO            GPIOE
+#define LED_G_PIN             26
+#define LED_G_ON()           (LED_G_GPIO->PCOR |= (1 << LED_G_PIN))
+#define LED_G_OFF()          (LED_G_GPIO->PSOR |= (1 << LED_G_PIN))
+#define LED_G_TOGGLE()       (LED_G_GPIO->PTOR |= (1 << LED_G_PIN))
+/*****************************************************************************/
+
+/*********************************** TASKS ***********************************/
+// Task Start
 #define TASKSTART_STK_SIZE 		512u
 #define TASKSTART_PRIO 			2u
 static OS_TCB TaskStartTCB;
 static CPU_STK TaskStartStk[TASKSTART_STK_SIZE];
 
-/* Task 2 */
+// Task 2
 #define TASK2_STK_SIZE			256u
 #define TASK2_STK_SIZE_LIMIT	(TASK2_STK_SIZE / 10u)
 #define TASK2_PRIO              3u
 static OS_TCB Task2TCB;
 static CPU_STK Task2Stk[TASK2_STK_SIZE];
 
-/* Example semaphore */
+// Task 3
+#define TASK3_STK_SIZE			256u
+#define TASK3_STK_SIZE_LIMIT	(TASK3_STK_SIZE / 10u)
+#define TASK3_PRIO              3u
+static OS_TCB Task3TCB;
+static CPU_STK Task3Stk[TASK3_STK_SIZE];
+/*****************************************************************************/
+
+/********************************* SEMAFORO **********************************/
 static OS_SEM semTest;
+/*****************************************************************************/
+
+
+//os multi
+/**************************** TASKS FUNCTIONS ********************************/
 
 static void Task2(void *p_arg) {
     (void)p_arg;
     OS_ERR os_err;
 
     while (1) {
-        OSSemPost(&semTest, OS_OPT_POST_1, &os_err);
-        OSTimeDlyHMSM(0u, 0u, 0u, 500u, OS_OPT_TIME_HMSM_STRICT, &os_err);
-        LED_R_TOGGLE();
+    	OSSemPost(&semTest, OS_OPT_POST_1, &os_err);
+		OSTimeDlyHMSM(0u, 0u, 0u, 500u, OS_OPT_TIME_HMSM_STRICT, &os_err);
+		LED_R_TOGGLE();
     }
 }
 
-
-static void TaskStart(void *p_arg) {
+static void Task3(void *p_arg) {
     (void)p_arg;
     OS_ERR os_err;
 
-    /* Initialize the uC/CPU Services. */
+    while (1) {
+    	OSSemPend(&semTest, 0, OS_OPT_PEND_BLOCKING, NULL, &os_err);
+        LED_B_TOGGLE();
+    }
+}
+
+static void TaskStart(void *p_arg)
+{
+    (void)p_arg;
+    OS_ERR os_err;
+
+    // Inicializo los uC/CPU Services
     CPU_Init();
 
+/*************************************************************/
 #if OS_CFG_STAT_TASK_EN > 0u
     /* (optional) Compute CPU capacity with no task running */
     OSStatTaskCPUUsageInit(&os_err);
@@ -64,11 +98,12 @@ static void TaskStart(void *p_arg) {
 #ifdef CPU_CFG_INT_DIS_MEAS_EN
     CPU_IntDisMeasMaxCurReset();
 #endif
+/************************************************************/
 
-    /* Create semaphore */
+    // Creo el semaforo
     OSSemCreate(&semTest, "Sem Test", 0u, &os_err);
 
-    /* Create Task2 */
+    // Creo el Task 2
     OSTaskCreate(&Task2TCB, 			//tcb
                  "Task 2",				//name
                   Task2,				//func
@@ -83,13 +118,41 @@ static void TaskStart(void *p_arg) {
                  (OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),
                  &os_err);
 
+    // Creo el Task 3
+        OSTaskCreate(&Task3TCB, 			//tcb
+                     "Task 3",				//name
+                      Task3,				//func
+                      0u,					//arg
+                      TASK3_PRIO,			//prio
+                     &Task3Stk[0u],			//stack
+                      TASK3_STK_SIZE_LIMIT,	//stack limit
+                      TASK3_STK_SIZE,		//stack size
+                      0u,
+                      0u,
+                      0u,
+                     (OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),
+                     &os_err);
+
     while (1) {
-        OSTimeDlyHMSM(0u, 0u, 0u, 1000u, OS_OPT_TIME_HMSM_STRICT, &os_err);
+        OSTimeDlyHMSM(0u, 0u, 2, 0u, OS_OPT_TIME_HMSM_STRICT, &os_err);
         LED_G_TOGGLE();
     }
 }
 
-int main(void) {
+/*****************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+int main5(void) {
     OS_ERR err;
 
 #if (CPU_CFG_NAME_EN == DEF_ENABLED)
